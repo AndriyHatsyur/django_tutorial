1f616ca3b66514e984efbf54ed1b6a664883eab5
Andriy Hatsyur <kanban6@gmail.com>
2019-11-06 20:43:32 +0200

    part 03

--- code/polls/urls.py
+++ code/polls/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('question/<int:id>', views.detail, name='detail'),
+    path('result/<int:id>', views.result, name='result'),
+    path('vote/<int:id>', views.vote, name='vote')
 ]
--- code/polls/views.py
+++ code/polls/views.py
@@ -1,4 +1,31 @@
 from django.http import HttpResponse
+from .models import Question
+from django.shortcuts import render, get_object_or_404
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+
+    question_list = Question.objects.order_by('-pub_date')[:5]
+    
+    context = {
+        'question_list': question_list,
+    }
+
+    return render(request, 'polls/index.html', context)
+
+    
+
+def detail(request, id):
+    question = get_object_or_404(Question, pk=id)
+
+    context = {
+        'question': question,
+    }
+
+    return render(request, 'polls/detail.html', context)
+    
+def result(request, id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % id)
+
+def vote(request, id):
+    return HttpResponse("You're voting on question %s." % id)
